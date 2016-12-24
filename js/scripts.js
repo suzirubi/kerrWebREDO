48db95ff9b47092f1901a0fd4889256c47e89aa2
Suzi Rubino <suzi@suzirubi.com>
2016-12-23 22:04:11 -0800

    created new folder without video

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+});
+$(document).ready(function() {
+  $(".dropdown-button").click(function() {
+    var $button, $menu;
+    $button = $(this);
+    $menu = $button.siblings(".dropdown-menu");
+    $menu.toggleClass("show-menu");
+    $menu.children("li").click(function() {
+      $menu.removeClass("show-menu");
+      $button.html($(this).html());
+    });
+  });
+});
